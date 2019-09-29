4c857d35136bb6af3b0ac208a5d1fa4b9d259dfd
Bigyo <b02902131@ntu.edu.tw>
2019-09-29 23:55:25 +0800

    Solve problem 8 and add LCSolutionV2

--- 8. String to Integer (atoi)/Problem-8.cs
+++ 8. String to Integer (atoi)/Problem-8.cs
@@ -6,20 +6,39 @@ namespace problems
 {
     public class LCProblem8 : ILCProblem
     {
-        LCTester<string, int> m_Tester;
+        LCTesterV2<string, int> m_Tester;
         public LCProblem8() { }
 
         public void PrepareTester()
         {
-            m_Tester = new LCTester<string, int>();
+            m_Tester = new LCTesterV2<string, int>();
         }
         public void AddTestCase()
         {
+            m_Tester.AddTestCase("", 0);
+            m_Tester.AddTestCase(" 2", 2);
+            m_Tester.AddTestCase("a", 0);
+            m_Tester.AddTestCase("+", 0);
+            m_Tester.AddTestCase("-", 0);
+            m_Tester.AddTestCase("-+1", 0);
+            m_Tester.AddTestCase("+42", 42);
             m_Tester.AddTestCase("42", 42);
+            m_Tester.AddTestCase(" 42", 42);
+            m_Tester.AddTestCase("   -42", -42);
             m_Tester.AddTestCase("-42", -42);
             m_Tester.AddTestCase("4193 with words", 4193);
             m_Tester.AddTestCase("words and 987", 0);
+            m_Tester.AddTestCase("2147483646", 2147483646);
+            m_Tester.AddTestCase("2147483647", 2147483647);
+            m_Tester.AddTestCase("2147483648", 2147483647);
+            m_Tester.AddTestCase("91283472332", 2147483647);
+            m_Tester.AddTestCase("-2147483647", -2147483647);
+            m_Tester.AddTestCase("-2147483648", -2147483648);
+            m_Tester.AddTestCase("-2147483649", -2147483648);
+            m_Tester.AddTestCase("2147483800", 2147483647);
             m_Tester.AddTestCase("-91283472332", -2147483648);
+            m_Tester.AddTestCase("  0000000000012345678", 12345678);
+            m_Tester.AddTestCase("-000000000000001", -1);
         }
 
         public void SetSolution(int solutionIndex)
