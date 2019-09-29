cd1bf6924d4b7e82637763ee9243803deeccf133
Bigyo <b02902131@ntu.edu.tw>
2019-09-29 16:20:27 +0800

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCodeCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
